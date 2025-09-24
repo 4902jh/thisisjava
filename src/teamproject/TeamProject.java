a540672f5da4a68ede86de8469289ddef0872598
seojaehee <seojaehee@localhost>
2025-09-24 11:21:36 +0900

    teamproject1

--- src/teamproject/TeamProject.java
+++ src/teamproject/TeamProject.java
@@ -2,7 +2,7 @@ package teamproject;
 
 import java.util.Scanner;
 
-public class TeamProject1 {
+public class TeamProject {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int moeny = 0;
